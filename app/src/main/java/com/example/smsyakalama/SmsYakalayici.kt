a67286eb63c31825e5a2e695ee8bfa83e890c3a7
Honor13 <onury.karaduman@gmail.com>
2023-07-26 19:09:30 +0300

    Android Studio (Kotlin) SMS content receive

--- app/src/main/java/com/example/smsyakalama/SmsYakalayici.kt
+++ app/src/main/java/com/example/smsyakalama/SmsYakalayici.kt
@@ -4,7 +4,6 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.os.Build
-import android.os.Bundle
 import android.telephony.SmsMessage
 import android.widget.Toast
 
