6ef3df6c5baf01cd4e9210543329cc65ac842483
Honor13 <onury.karaduman@gmail.com>
2023-07-26 19:08:42 +0300

    Android Studio (Kotlin) SMS content receive

---
+++ app/src/main/java/com/example/smsyakalama/SmsYakalayici.kt
@@ -0,0 +1,41 @@
+package com.example.smsyakalama
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import android.telephony.SmsMessage
+import android.widget.Toast
+
+class SmsYakalayici : BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+
+
+        val b = intent?.extras
+
+        if (b != null) {
+            val pdusObj = b.get("pdus") as Array<Any>
+
+            for (i in pdusObj.indices) {
+
+                val guncelMesaj: SmsMessage
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) { // version kontrolü
+                    val format = b.getString("format")
+                    guncelMesaj = SmsMessage.createFromPdu(pdusObj[i] as ByteArray, format)
+                } else {
+                    guncelMesaj = SmsMessage.createFromPdu(pdusObj[i] as ByteArray)
+                }
+                val telNo = guncelMesaj.displayOriginatingAddress
+                val mesaj = guncelMesaj.displayMessageBody
+
+                Toast.makeText(context, "$telNo - $mesaj", Toast.LENGTH_SHORT).show()
+
+
+            }
+        }
+    }
+}
+
+
